35833037ec62e571f70cb5be2d6c9e0739fea7bb
mayue <mayue@mayuedeMacBook-Air.local>
2017-01-16 14:59:45 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
 
@@ -14,7 +16,7 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
-  
+
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
